6612d6806db74b71415103007b69588741f40a3c
Hyemi Lee <g11lee@torontomu.ca>
2023-12-04 20:51:54 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div style={{margin:"auto", width:"700px", border:"3px solid rgb(107, 200, 120)", borderRadius:"35px", padding:"10px 50px 10px"}}>
+      <header style={{color: "red"}}>
         <p>
-          I first installed Node.js and npm from their designated websites, then used the command "npx create-react-app 'cps530-lab10'" in the command line, allowing me to create a React application folder in my local directory. 
-          I then edited the source file to design this webpage.
-          Afterwards, I went on Github to create a repository to host the webpage I creatd. 
+          I first installed Node.js and npm from their designated websites, then used the command "npx create-react-app 'cps530-lab10'" in the command line, allowing me to create a React application folder in my local directory. I then edited the source file to design this webpage. Afterwards, I went on Github to create a repository to host the webpage I creatd. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
